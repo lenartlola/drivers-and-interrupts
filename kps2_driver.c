bb1c1e37c1ce0185ec9653adcea371a88d6ebb7e
lenart <lenartlola@proton.me>
2024-04-27 16:48:32 +0200

    Add US set table

--- kps2_driver.c
+++ kps2_driver.c
@@ -24,6 +24,46 @@ typedef enum {
 
 Modifiers gmodifiers = None;
 
+const unsigned char kbdus[128] =
+{
+	0,  27, '1', '2', '3', '4', '5', '6', '7', '8',		/* 9 */
+	'9', '0', '-', '=', '\b',				/* Backspace */
+	'\t',							/* Tab */
+	'q', 'w', 'e', 'r',					/* 19 */
+	't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',		/* Enter key */
+	0,							/* 29   - Control */
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+	'\'', '`',   0,						/* Left shift */
+	'\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+	'm', ',', '.', '/',   0,				/* Right shift */
+	'*',
+	0,							/* Alt */
+	' ',							/* Space bar */
+	0,							/* Caps lock */
+	0,							/* 59 - F1 key ... > */
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,							/* < ... F10 */
+	0,							/* 69 - Num lock*/
+	0,							/* Scroll Lock */
+	0,							/* Home key */
+	0,							/* Up Arrow */
+	0,							/* Page Up */
+	'-',
+	0,							/* Left Arrow */
+	0,
+	0,							/* Right Arrow */
+	'+',
+	0,							/* 79 - End key*/
+	0,							/* Down Arrow */
+	0,							/* Page Down */
+	0,							/* Insert Key */
+	0,							/* Delete Key */
+	0,   0,   0,
+	0,							/* F11 Key */
+	0,							/* F12 Key */
+	0,							/* All other keys are undefined */
+};
+
 static int check_for_modifiers(unsigned char scancode)
 {
 	switch (scancode) {
@@ -68,18 +108,16 @@ RETURN_SUCCESS:
 static irqreturn_t irq_handler(int irq, void* dev_id)
 {
 	static unsigned char 	scancode;
-	int			mod_state;
+	char			c;
 
 	scancode = inb(0x60);
-	mod_state = check_for_modifiers(scancode);
-	if (gmodifiers & LShift) {
-		printk(KERN_INFO "kps2: Detected RShift pressed\n");
+	if (!check_for_modifiers(scancode))
 		goto FINISH;
-	} else {
-		printk(KERN_INFO "kps2: RShift not pressed or released");
+	c = kbdus[scancode];
+	if (c >= 0x20 && c <= 0x7e) {
+		printk(KERN_INFO "ps2: key has been pressed: %c\n", c);
 	}
 
-	printk(KERN_INFO "Pressed a normal key %d\n", scancode);
 FINISH:
 	return IRQ_NONE;
 }
